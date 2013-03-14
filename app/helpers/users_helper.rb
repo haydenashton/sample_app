0681f2a501969dbfd8d3d5b6efc02967956a9171
Hayden Ashton <haydenashton@hotmail.com>
2013-03-14 20:36:58 +1300

    Fix Gravatar urls

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,8 +7,8 @@ module UsersHelper
 
     size = options[:size]
 
-    gravatar_url = "https://secure.gravatar.com/avatars/#{gravatar_id}.png?s=#{size}"
-
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+#gravatar_url = "http://www.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
     image_tag(gravatar_url, :alt => user.name, :class => "gravatar")
 
   end
